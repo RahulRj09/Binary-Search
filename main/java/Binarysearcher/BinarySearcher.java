b45af4a02760ea0574372d9ab4f5c2fa09a8dfef
RahulRj09 <rahul18@navgurukul.org>
2019-05-20 21:33:25 +0530

    refactor binary search function

--- main/java/Binarysearcher/BinarySearcher.java
+++ main/java/Binarysearcher/BinarySearcher.java
@@ -5,20 +5,18 @@ public class BinarySearcher{
         if(firstIndex <= lastIndex){
             int middileIndex = (firstIndex + lastIndex) / 2;
             if(numbers[middileIndex] < userInput){
-                firstIndex = middileIndex + 1;
-            return numberChecker(userInput, numbers, firstIndex, lastIndex);
+                return numberChecker(userInput, numbers, middileIndex+1, lastIndex);
             }else if (numbers[middileIndex]== userInput){
                 return middileIndex;
             }else {
-                lastIndex = middileIndex -1;
-                return numberChecker(userInput, numbers, firstIndex, lastIndex);
+                return numberChecker(userInput, numbers, middileIndex-1, lastIndex);
             }
         }
         return -1;
     }
 
     public static void main(String[] args){
-        int userInput = 20;
+        int userInput = 60;
         int numbers[] = {10,20,30,40,50};
         int lastIndex = numbers.length - 1;
         System.out.println(numberChecker(userInput, numbers, 0, lastIndex));

f3b8421019fac68f925218047e472a2c01f3b23a
RahulRj09 <rahul18@navgurukul.org>
2019-06-10 15:34:57 +0530

    add. test cases

--- test/java/number/NumberTest.java
+++ test/java/number/NumberTest.java
@@ -21,14 +21,14 @@ public class NumberTest {
         assertEquals(-1, BinarySearcher.elementFinder(60, numbers, 0, lastIndex));
     }
     @Test
-    public void theStringsShouldBeExistInTheList(){
+    public void theStringShouldBeExistInTheList(){
         List<String> strings = Arrays.asList("a", "b", "c", "d", "e");
         int lastIndex =strings.size() - 1;
         assertEquals(0, BinarySearcher.elementFinder("a", strings, 0, lastIndex));
     }
     @Test
     public void theDecimalNumberShouldBeExistInTheList(){
-        List<Double> numbers = Arrays.asList(10.1,20.2,30.3,40.5);
+        List<Double> numbers = Arrays.asList(10.1,20.2,30.3,40.4);
         int lastIndex = numbers.size() - 1;
         assertEquals(2, BinarySearcher.elementFinder(30.3, numbers, 0, lastIndex));
     }

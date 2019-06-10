f59f04ffc2002cfcd33f66107422d0fbafd876bd
RahulRj09 <rahul18@navgurukul.org>
2019-06-10 14:54:06 +0530

    add. generic binarySearch

--- main/java/Binarysearcher/BinarySearcher.java
+++ main/java/Binarysearcher/BinarySearcher.java
@@ -1,25 +1,31 @@
 package Binarysearcher;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BinarySearcher{
-    public static int numberChecker(int userInput, int[] numbers, int firstIndex, int lastIndex) {
+    private static <T extends Comparable<T>> int elementFinder(T userInput, List<T> elements, int firstIndex, int lastIndex) {
         if(firstIndex <= lastIndex){
             int middileIndex = (firstIndex + lastIndex) / 2;
-            if(numbers[middileIndex] < userInput){
-                return numberChecker(userInput, numbers, middileIndex+1, lastIndex);
-            }else if (numbers[middileIndex]== userInput){
+            if(elements.get(middileIndex).compareTo(userInput) < 0){
+                return elementFinder(userInput, elements, middileIndex+1, lastIndex);
+            }else if (elements.get(middileIndex).equals(userInput)){
                 return middileIndex;
-            }else {
-                return numberChecker(userInput, numbers, middileIndex-1, lastIndex);
             }
+            return elementFinder(userInput, elements, 0, middileIndex-1);
         }
         return -1;
     }
 
     public static void main(String[] args){
-        int userInput = 60;
-        int numbers[] = {10,20,30,40,50};
-        int lastIndex = numbers.length - 1;
-        System.out.println(numberChecker(userInput, numbers, 0, lastIndex));
+        int userInput = 10;
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+        int lastIndex = numbers.size() - 1;
+        String userInput1 = "c";
+        List<String> words = Arrays.asList("a", "b", "c", "d", "e");
+        int lastIndex1 = numbers.size() - 1;
+        System.out.println(elementFinder(userInput, numbers, 0, lastIndex));
+        System.out.println(elementFinder(userInput1, words, 0, lastIndex1));
     }
 }
 
